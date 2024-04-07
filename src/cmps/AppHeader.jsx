cff213194630eeac1cece5d2fecb961c88463bb2
Mor Marzan <mormarzan@gmail.com>
2024-04-07 13:39:17 +0300

    Add sticky header logic and style

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,13 +1,28 @@
+import { useEffect, useState } from "react"
 import { NavLink, useNavigate } from "react-router-dom"
+
 import logo from '/images/logo.svg'
 import hamburger from '/images/icon-hamburger.svg'
 
 
 export function AppHeader() {
 
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
+    function handleScroll() {
+        setIsScrolled(window.scrollY > 0)
+    }
+
+    const dynClass = isScrolled ? 'scrolled' : ''
 
     return (
-        <header className="main-layout full app-header ">
+        <header className={`main-layout full app-header ${dynClass}`}>
 
             <div className="section-container flex align-center">
                 <img src={logo} alt="myteam logo" className="logo"></img>
